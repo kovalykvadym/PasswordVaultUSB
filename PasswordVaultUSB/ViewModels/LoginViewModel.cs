39c43b318efeab7fc4e02e2ef19d894e1507ae1c
Vadym Kovalyk <215728571+kovalykvadym@users.noreply.github.com>
2025-12-01 05:25:34 +0200

    ref: Refactoring code for login view

--- PasswordVaultUSB/ViewModels/LoginViewModel.cs
+++ PasswordVaultUSB/ViewModels/LoginViewModel.cs
@@ -1,12 +1,162 @@
-﻿using System;
+﻿using PasswordVaultUSB.Helpers;
+using PasswordVaultUSB.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace PasswordVaultUSB.ViewModels
 {
-    internal class LoginViewModel
+    internal class LoginViewModel : BaseViewModel
     {
+
+        private string _username;
+        private string _password;
+        private bool _isPasswordVisible;
+        private string _usernameError;
+        private string _passwordError;
+        private bool _hasUsernameError;
+        private bool _hasPasswordError;
+
+        public string Username
+        {
+            get => _username; 
+            set
+            {
+                if (SetProperty(ref _username, value))
+                {
+                    if (HasUsernameError)
+                    {
+                        HasUsernameError = false;
+                        UsernameError = string.Empty;
+                    }
+                }
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (SetProperty(ref _password, value))
+                {
+                    if (HasPasswordError)
+                    {
+                        HasPasswordError = false;
+                        PasswordError = string.Empty;
+                    }
+                }
+            }
+        }
+
+        public bool IsPasswordVisible
+        {
+            get => _isPasswordVisible;
+            set => SetProperty(ref _isPasswordVisible, value);
+        }
+
+        public string UsernameError
+        {
+            get => _usernameError;
+            set => SetProperty(ref _usernameError, value);
+        }
+
+        public string PasswordError
+        {
+            get => _passwordError;
+            set => SetProperty(ref _passwordError, value);
+        }
+
+        public bool HasUsernameError
+        {
+            get => _hasUsernameError; 
+            set => SetProperty(ref _hasUsernameError, value);
+        }
+
+        public bool HasPasswordError
+        {
+            get => _hasPasswordError;
+            set => SetProperty(ref _hasPasswordError, value);
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand TogglePasswordVisibilityCommand { get; }
+        public ICommand NavigateToRegisterCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(ExecuteLogin);
+            TogglePasswordVisibilityCommand = new RelayCommand(_ =>  TogglePasswordVisibility());
+            NavigateToRegisterCommand = new RelayCommand(ExecuteNavigateToRegister);
+        }
+
+        private void ExecuteLogin(object parameter)
+        {
+            ResetErrors();
+
+            bool hasError = false;
+
+            if (string.IsNullOrWhiteSpace(Username))
+            {
+                UsernameError = "Please enter your username!";
+                HasUsernameError = true;
+                hasError = true;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                PasswordError = "Please enter your password!";
+                HasPasswordError = true;
+                hasError = true;
+            }
+            else if (Password != "1234")
+            {
+                PasswordError = "Incorrect password. Please try again.";
+                HasPasswordError = true;
+                hasError = true;
+            }
+
+            if (hasError)
+            {
+                return;
+            }
+
+            var mainView = new MainView();
+            Application.Current.MainWindow = mainView;
+            mainView.Show();
+            CloseWindow(parameter);
+        }
+
+        private void TogglePasswordVisibility()
+        {
+            IsPasswordVisible = !IsPasswordVisible;
+        }
+
+        private void ExecuteNavigateToRegister(object parameter)
+        {
+            var registerView = new RegisterView();
+            registerView.Show();
+            CloseWindow(parameter);
+        }
+
+        private void ResetErrors()
+        {
+            HasUsernameError = false;
+            HasPasswordError = false;
+            UsernameError = string.Empty;
+            PasswordError = string.Empty;
+        }
+
+        private void CloseWindow(object parameter)
+        {
+            if (parameter is Window window)
+            {
+                window.Close();
+            }
+        }
     }
 }
--- PasswordVaultUSB/Views/LoginView.xaml.cs
+++ PasswordVaultUSB/Views/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PasswordVaultUSB.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,116 +17,19 @@ namespace PasswordVaultUSB.Views
 {
     public partial class LoginView : Window
     {
-        private bool _isPasswordVisible = false;
+        private LoginViewModel ViewModel => DataContext as LoginViewModel;
         public LoginView()
         {
             InitializeComponent();
-        }
-
-        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                LoginButton_Click(sender, e);
-            }
-        }
-
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
-        {
-            ResetErrors();
-
-            string username = UsernameInput.Text;
-            string password = PasswordInput.Password;
-
-            bool hasError = false;
-
-            if (string.IsNullOrWhiteSpace(username))
-            {
-                UsernameErrorText.Text = "Please enter your username!";
-                UsernameErrorText.Visibility = Visibility.Visible;
-                hasError = true;
-            }
-
-            if (string.IsNullOrEmpty(password))
-            {
-                PasswordErrorText.Text = "Please enter your password!";
-                PasswordErrorText.Visibility = Visibility.Visible;
-                hasError = true;
-            }
-            else if(password != "1234")
-            {
-                PasswordErrorText.Text = "Incorrect password. Please try again.";
-                PasswordErrorText.Visibility = Visibility.Visible;
-                hasError = true;
-            }
-
-            if (hasError)
-            {
-                return;
-            }
-
-            var mainView = new MainView();
-            Application.Current.MainWindow = mainView;
-            mainView.Show();
-            this.Close();
-        }
-
-        private void ResetErrors()
-        {
-            UsernameErrorText.Visibility = Visibility.Collapsed;
-            PasswordErrorText.Visibility = Visibility.Collapsed;
-        }
-
-        private void TogglePasswordButton_Click(object sender, RoutedEventArgs e)
-        {
-            _isPasswordVisible = !_isPasswordVisible;
-            if (_isPasswordVisible)
-            {
-                PasswordVisible.Text = PasswordInput.Password;
-                PasswordVisible.Visibility = Visibility.Visible;
-                PasswordInput.Visibility = Visibility.Collapsed;
-
-                EyeIcon.Source = new BitmapImage(new Uri("/Resources/visibility_off.png", UriKind.Relative));
-            }
-            else
-            {
-                PasswordInput.Password = PasswordVisible.Text;
-                PasswordInput.Visibility = Visibility.Visible;
-                PasswordVisible.Visibility = Visibility.Collapsed;
-
-                EyeIcon.Source = new BitmapImage(new Uri("/Resources/visibility.png", UriKind.Relative));
-            }
+            PasswordInput.PasswordChanged += PasswordInput_PasswordChanged;
         }
 
         private void PasswordInput_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            if (!_isPasswordVisible)
+            if (ViewModel != null && !ViewModel.IsPasswordVisible)
             {
-                PasswordVisible.Text = PasswordInput.Password;
-                if (PasswordErrorText.Visibility == Visibility.Visible)
-                {
-                    PasswordErrorText.Visibility = Visibility.Collapsed;
-                }
+                ViewModel.Password = PasswordInput.Password;
             }
-        }
-
-        private void PasswordVisible_TextChanged(object sender, RoutedEventArgs e)
-        {
-            if (_isPasswordVisible)
-            {
-                PasswordInput.Password = PasswordVisible.Text;
-                if (PasswordErrorText.Visibility == Visibility.Visible)
-                {
-                    PasswordErrorText.Visibility = Visibility.Collapsed;
-                }
-            }
-        }
-
-        private void GoToRegister_Click(object sender, RoutedEventArgs e)
-        {
-            var registerView = new RegisterView();
-            registerView.Show();
-            this.Close();
         }
     }
 }
